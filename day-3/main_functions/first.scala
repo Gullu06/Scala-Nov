d238311c826358a85cfbad6a55c4ce15bbe03dbd
Priyanshi <priyanshichouhan0011@gmail.com>
2024-10-28 17:15:43 +0530

    Day-3 classwork and assingment

---
+++ day-3/main_functions/first.scala
@@ -0,0 +1,5 @@
+object FirstPro{
+    def main(args: Array[String]) = {
+        println("This is my first Scala program")
+    }
+}
---
+++ day-3/main_functions/fourth.scala
@@ -0,0 +1,3 @@
+@main def perform(name: String, age: Int): Unit = {
+    println(s"Hi! I'm $name, my age is $age!")
+}
