c3ffe28fdfba454fba3b2ef40acb6351bd8f6c10
Priyanshi <priyanshichouhan0011@gmail.com>
2024-11-11 11:37:53 +0530

    updated few files

--- day-9/Task-2/src/main/scala/KafkaConsumerFactory.scala
+++ day-9/Task-2/src/main/scala/KafkaConsumerFactory.scala
@@ -5,7 +5,7 @@ import java.util.Properties
 object KafkaConsumerFactory {
   def createConsumerProps(groupId: String): Properties = {
     val props = new Properties()
-    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092") // Ensure this matches your Kafka server
+    props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
     props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId)
     props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
     props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer")
--- day-9/Task-2/src/main/scala/MessageGatherer.scala
+++ day-9/Task-2/src/main/scala/MessageGatherer.scala
@@ -1,16 +1,18 @@
 import akka.actor.Actor
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord, RecordMetadata}
 
 class MessageGatherer(producer: KafkaProducer[String, String]) extends Actor {
   override def receive: Receive = {
     case msg: Message =>
       println(s"MessageGatherer received: ${msg.message} from ${msg.messageType}")
       val record = new ProducerRecord[String, String]("consolidated-messages", msg.messageKey, msg.message)
-      producer.send(record, (metadata, exception) => {
+
+      // Asynchronously send the message with a callback to handle success/failure
+      producer.send(record, (metadata: RecordMetadata, exception: Exception) => {
         if (exception != null) {
           println(s"Error sending message: ${exception.getMessage}")
         } else {
-          println(s"Message sent to consolidated-messages: ${msg.message}")
+          println(s"Message sent to consolidated-messages topic with offset ${metadata.offset()}: ${msg.message}")
         }
       })
   }
