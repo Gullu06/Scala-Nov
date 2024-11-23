f0ef904798cfd9de82fa89819b9efed79e83f8f7
Priyanshi <priyanshichouhan0011@gmail.com>
2024-11-23 12:21:20 +0530

    JWT authentication added

---
+++ Week3-Projects/visitorManagement/visitormanagementservice/app/security/JwtAuthFilter.scala
@@ -0,0 +1,40 @@
+package security
+
+import org.apache.pekko.stream.Materializer
+import play.api.mvc._
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class JwtAuthFilter @Inject()(
+                               implicit val mat: Materializer,
+                               ec: ExecutionContext
+                             ) extends Filter {
+  println("JwtAuthFilter initialized!") // Debug log
+
+  override def apply(nextFilter: RequestHeader => Future[Result])(request: RequestHeader): Future[Result] = {
+    val publicRoutes = Seq("/api/login", "/persons")
+
+    println(s"Processing request path: ${request.path}")
+
+    if (publicRoutes.exists(request.path.startsWith)) {
+      println("Public route detected. Skipping authentication.")
+      nextFilter(request)
+    } else {
+      val tokenOpt = request.headers.get("Authorization").map(_.replace("Bearer ", ""))
+      tokenOpt match {
+        case Some(token) =>
+          JwtUtil.validateToken(token) match {
+            case Some(userId) =>
+              println(s"Valid token for user: $userId")
+              nextFilter(request)
+            case None =>
+              println("Invalid or expired token.")
+              Future.successful(Results.Unauthorized("Invalid or missing token"))
+          }
+        case None =>
+          println("Authorization header missing.")
+          Future.successful(Results.Unauthorized("Authorization header missing or invalid."))
+      }
+    }
+  }
+}
--- Week3-Projects/visitorManagement/visitormanagementservice/build.sbt
+++ Week3-Projects/visitorManagement/visitormanagementservice/build.sbt
@@ -25,3 +25,14 @@ libraryDependencies ++= Seq(
   "org.apache.kafka" %% "kafka" % "3.0.0",
   "com.typesafe.play" %% "play-json" % "2.9.2"
 )
+
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-stream" % "2.6.20", // Akka Streams
+  "com.typesafe.akka" %% "akka-actor" % "2.6.20",  // Akka Actor
+  "com.typesafe.akka" %% "akka-slf4j" % "2.6.20",
+  "org.apache.pekko" %% "pekko-stream" % "1.0.1",
+  "com.auth0" % "java-jwt" % "4.3.0", // Java JWT library
+  "com.typesafe.play" %% "play-json" % "2.9.4" // Play JSON for JSON processing
+)
+
+libraryDependencies += filters
